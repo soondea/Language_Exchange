88294711b47f552588d763ae25e04fd896b96a6c
soondea <soondea@gmail.com>
2016-08-22 15:01:12 +0900

    zzz

---
+++ Language_Exchange/src/member/model/member.java
@@ -0,0 +1,77 @@
+package member.model;
+
+public class member {
+	
+	private String name;
+	private String password;
+	private String email;
+	private String mother_language;
+	private String target_language;
+	private String skype;
+	private String intro;
+	private String ability;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMother_language() {
+		return mother_language;
+	}
+
+	public void setMother_language(String mother_language) {
+		this.mother_language = mother_language;
+	}
+
+	public String getTarget_language() {
+		return target_language;
+	}
+
+	public void setTarget_language(String target_language) {
+		this.target_language = target_language;
+	}
+
+	public String getSkype() {
+		return skype;
+	}
+
+	public void setSkype(String skype) {
+		this.skype = skype;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	public String getAbility() {
+		return ability;
+	}
+
+	public void setAbility(String ability) {
+		this.ability = ability;
+	}
+}
